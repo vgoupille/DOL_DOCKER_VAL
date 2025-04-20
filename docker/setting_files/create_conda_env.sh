a5a8677173b55368468ef0c6149b056b7e78e6fd
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2025-04-20 09:40:45 +0200

    Update development environment configuration: enhance Jupyter settings, streamline Dockerfile commands, and add initial test notebook for R functionality.

--- docker/setting_files/create_conda_env.sh
+++ docker/setting_files/create_conda_env.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# Initialize conda
+eval "$(conda shell.bash hook)"
+
 # Create and activate the environment
-conda env create -f renv1.yml
-echo "conda activate renv1" >> ~/.bashrc
+conda env create -f pkgs/env_1.yml
+echo "conda activate env_1" >> ~/.bashrc
 
 # Verify installation
-conda activate renv1
+conda activate env_1
 python --version
-radian --version 
